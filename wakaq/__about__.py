686ab8e711a4222958f044cabbf023fe9cb2a408
Alan Hamlett <alan.hamlett@gmail.com>
2023-10-11 06:56:10 +0200

    v2.1.3

--- wakaq/__about__.py
+++ wakaq/__about__.py
@@ -1,7 +1,7 @@
 __title__ = "WakaQ"
 __description__ = "Background task queue for Python backed by Redis, a minimal Celery."
 __url__ = "https://github.com/wakatime/wakaq"
-__version_info__ = ("2", "1", "2")
+__version_info__ = ("2", "1", "3")
 __version__ = ".".join(__version_info__)
 __author__ = "Alan Hamlett"
 __author_email__ = "alan.hamlett@gmail.com"
